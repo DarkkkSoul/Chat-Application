1d31a3f34a2a30bd9c2db4a10e5fb6d0c840f495
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-10-02 19:32:23 +0530

    making ws server in backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+// console.log(io);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  console.log(socket);
+});
+
+server.listen(8989, () => {
+  console.log('server running at http://localhost:8989');
+});
